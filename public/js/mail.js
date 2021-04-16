f951ec6b4b7e078ac98fa8fb3f4fd2d0b5895f81
Navu4 <51900952+Navu4@users.noreply.github.com>
2021-04-17 00:21:04 +0530

    Update

--- public/js/mail.js
+++ public/js/mail.js
@@ -1,149 +1,59 @@
 const puppy = require('puppeteer');
 
-function main() {
-  let { emailId, pwd } = require('.//myInfo.json');
-  // let {
-  //   senderEmailId,
-  //   mailSubject,
-  //   mailText,
-  // } = require('./senderDetials.json');
+let { emailId, pwd } = require('./myInfo.json');
+let { senderEmailId, mailSubject, mailText } = require('./senderDetials.json');
 
-  const mydata = JSON.parse(localStorage.data);
-  const { senderEmailId, mailSubject, mailText } = mydata;
+async function timeout(time) {
+  return new Promise(function (resolve, reject) {
+    // Asynchronous setTimeout function
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+}
 
-  let tab; // Working tab of the browser
-  let browserPromise = puppy.launch({
+async function main() {
+  let browser = await puppy.launch({
     headless: false,
     defaultViewport: false,
     args: ['--start-maximized'],
   });
 
-  let brow;
-  browserPromise
-    .then(function (browser) {
-      brow = browser;
-      let pagesPromise = browser.pages();
-      return pagesPromise;
-    })
-    .then(function (pages) {
-      tab = pages[0];
-      // Open the login page of gmail
-      let pageOpenPromise = tab.goto(
-        'https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin'
-      );
-      return pageOpenPromise;
-    })
-    .then(function () {
-      let waitPromise = tab.waitForSelector('.whsOnd.zHQkBf ', {
-        visible: true,
-      });
-      return waitPromise;
-    })
-    .then(function () {
-      let allclassPromise = tab.click('.whsOnd.zHQkBf'); // click on email button
-      return allclassPromise;
-    })
-    .then(function () {
-      let idPromise = tab.type('#identifierId', emailId); // type email ID
-      return idPromise;
-    })
-    .then(function () {
-      let nextbtnPromise = tab.click('.VfPpkd-RLmnJb'); // Click on the next button
-      return nextbtnPromise;
-    })
-    .then(function () {
-      return new Promise(function (resolve, reject) {
-        // Asynchronous setTimeout function
-        setTimeout(() => {
-          resolve();
-        }, 3000);
-      });
-    })
-    .then(function () {
-      let pwdclassPromise = tab.click('.whsOnd.zHQkBf'); // click on password
-      return pwdclassPromise;
-    })
-    .then(function () {
-      let pwdPromise = tab.type('.whsOnd.zHQkBf', pwd); // type the password
-      return pwdPromise;
-    })
-    .then(function () {
-      let nextbtnPromise = tab.click('.VfPpkd-RLmnJb'); // Click on the next sign in button
-      return nextbtnPromise;
-    })
-    .then(function () {
-      let composeMailWaitPromise = tab.waitForSelector('.T-I.T-I-KE.L3');
-      return composeMailWaitPromise;
-    })
-    .then(function () {
-      let composeMailPromise = tab.click('.T-I.T-I-KE.L3');
-      return composeMailPromise;
-    })
-    .then(function () {
-      return new Promise(function (resolve, reject) {
-        setTimeout(() => {
-          resolve();
-        }, 5000);
-      });
-    })
-    .then(function () {
-      let toWaitPrimise = tab.waitForSelector('.wO.nr.l1', { visible: true }); // after clicking on compose waiting for box to appear
-      return toWaitPrimise;
-    })
-    .then(function () {
-      let toPromise = tab.click('.wO.nr.l1'); // after box will appear click on To/Receipient
-      return toPromise;
-    })
-    .then(function () {
-      let toTypePromise = tab.type('.wO.nr.l1', senderEmailId, { delay: 200 }); //type the mail id of the receipient
-      return toTypePromise;
-    })
-    .then(function () {
-      let enterPromise = tab.keyboard.press('Enter'); //by clicking enter it will be selected
-      return enterPromise;
-    })
-    .then(function () {
-      let subjectclickPromise = tab.click('.aoT'); // this is for subject cc
-      return subjectclickPromise;
-    })
-    .then(function () {
-      let subjectPromise = tab.type('.aoT', mailSubject, { delay: 100 }); //  type the message in subject / cc
-      return subjectPromise;
-    })
-    .then(function () {
-      let textAreaClickPromise = tab.click('.Am.Al.editable.LW-avf.tS-tW'); // click on the text area where we need to type our message
-      return textAreaClickPromise;
-    })
-    .then(function () {
-      let textAreaPromise = tab.type('.Am.Al.editable.LW-avf.tS-tW', mailText, {
-        delay: 300,
-      }); // getting the text from Json file for typing message in compose-mail box
-      return textAreaPromise;
-    })
-    .then(function () {
-      let sendPromise = tab.click('.T-I.J-J5-Ji.aoO.v7.T-I-atl.L3'); // click on send button
-      return sendPromise;
-    })
-    .then(function () {
-      let waitIdPromise = tab.waitForSelector('.gb_Da.gbii', { visible: true });
-      return waitIdPromise;
-    })
-    .then(function () {
-      let profilePromise = tab.click('.gb_Da.gbii');
-      return profilePromise;
-    })
-    .then(function () {
-      return new Promise(function (resolve, reject) {
-        setTimeout(() => {
-          resolve();
-        }, 7000);
-      }); // waiting for completion of sending file
-    })
-    .then(function () {
-      let signoutPromise = tab.click('.gb_Db.gb_Vf.gb_4f.gb_Re.gb_4c'); // clicking on signout
-      return signoutPromise;
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
+  let pages = await browser.pages();
+  let tab = pages[0];
+  await tab.goto(
+    'https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin'
+  );
+  
+//   await tab.waitForNavigation({waitUntil:"networkidle0"});
+  await tab.waitForSelector('.whsOnd.zHQkBf ', { visible: true });
+  await tab.click('.whsOnd.zHQkBf'); // click on email button
+  await tab.type('#identifierId', emailId); // type email ID
+//   await timeout(3000);
+  await tab.click('.VfPpkd-RLmnJb'); // Click on the next button
+  await timeout(3000);
+  await tab.click('.whsOnd.zHQkBf'); // click on password
+  await tab.type('.whsOnd.zHQkBf', pwd); // type the password
+  await tab.click('.VfPpkd-RLmnJb'); // Click on the next sign in button
+  await tab.waitForSelector('.T-I.T-I-KE.L3');
+  await tab.click('.T-I.T-I-KE.L3');
+  await timeout(5000);
+  await tab.waitForSelector('.wO.nr.l1', { visible: true }); // after clicking on compose waiting for box to appear
+  await tab.click('.wO.nr.l1'); // after box will appear click on To/Receipient
+  await tab.type('.wO.nr.l1', senderEmailId, { delay: 200 }); //type the mail id of the receipient
+  await tab.keyboard.press('Enter'); //by clicking enter it will be selected
+  await tab.click('.aoT'); // this is for subject cc
+  await tab.type('.aoT', mailSubject, { delay: 100 }); //  type the message in subject / cc
+  await tab.click('.Am.Al.editable.LW-avf.tS-tW'); // click on the text area where we need to type our message
+  await tab.type('.Am.Al.editable.LW-avf.tS-tW', mailText, {
+    delay: 300,
+  }); // getting the text from Json file for typing message in compose-mail box
+  await tab.click('.T-I.J-J5-Ji.aoO.v7.T-I-atl.L3'); // click on send button
+  await tab.waitForSelector('.gb_Da.gbii', { visible: true });
+  await  tab.click('.gb_Da.gbii');
+  await timeout(7000);
+  await tab.click('.gb_Db.gb_Vf.gb_4f.gb_Re.gb_4c'); // clicking on signout
+  await browser.close();
 }
+
+main();
--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,14 @@ const btn = document.getElementById('btn');
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 
-// Start
+// FinalData for Mail Json file containing senderEmailId, mailSubject, mailContent
+let finalData = {};
+
+// Meeting data
+let meetingData = {};
+
+// Start Listening
+// Open mic of the system and listen to the first command
 function startListening() {
   let recog = new SpeechRecognition();
   recog.start();
@@ -17,15 +24,26 @@ function startListening() {
   };
 }
 
-function mailingListen() {
-  let recog = new SpeechRecognition();
-  recog.start();
-  recog.onstart = console.log('Started Listening .. ');
+// Listen to different content of Mail and meeting
+function Listen() {
+  const ListeningPromise = new Promise((resolve, rejects) => {
+    let recog = new SpeechRecognition();
+    recog.start();
+    recog.onstart = console.log('Started Listening');
 
-  recog.onresult = function (data) {
-    return data;
-  };
+    recog.onspeechend = function () {
+      recog.stop();
+    };
+
+    recog.onresult = function (data) {
+      resolve(data);
+    };
+  });
+
+  return ListeningPromise;
 }
+
+// ----------------------------------------------------------------
 
 // Speak Text Entered
 function Speak(TEXT) {
@@ -35,7 +53,9 @@ function Speak(TEXT) {
   window.speechSynthesis.speak(utter);
 }
 
-// Handle Results
+// -------------------------------------------------------------------
+
+// Handle Results/Data
 function handleResults(data) {
   let text = data.results[0][0].transcript;
   text = text.toLowerCase();
@@ -43,19 +63,14 @@ function handleResults(data) {
 
   ProcessCommand(text);
 }
+
+// Processes the User Command and work accordingly
 function ProcessCommand(UserText) {
   if (UserText.includes('mail') || UserText.includes('Email')) {
     mailContent();
-
   } else if (UserText.includes('meeting') || UserText.includes('meet')) {
     Speak('Arranging a google meet');
-    recordContent(4);
-    setTimeout(() => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('POST', '/meeting', true);
-      xhr.send({ message: 'we are arranging a meet' });
-    }, 3000);
-
+    meetingContent(UserText);
   } else if (UserText.includes('instagram')) {
     Speak('Opening Instagram...');
     window.open('https://www.instagram.com/');
@@ -67,12 +82,13 @@ function ProcessCommand(UserText) {
     window.open('https://www.google.com/');
   } else if (UserText.includes('the') && UserText.includes('time')) {
     console.log('The time is :' + getCurrentTime());
-  } else if (UserText.includes('mail') || UserText.includes('mail')) {
-    Speak('Please...Tell the sender Mail Id');
   } else {
     console.log('I cant perform task : $(text)');
+    Speak('I cant perform task');
   }
 }
+
+// -------------------------------------------------------------------
 
 // Current Time Function
 function getCurrentTime() {
@@ -82,6 +98,8 @@ function getCurrentTime() {
   let minutes = date.getMinutes();
   let currentTimeIs = `${hour} ${minutes}`;
   Speak('The Time Is... ' + currentTimeIs);
+
+  return currentTimeIs;
 }
 
 // Intro Lines of Start Button
@@ -91,83 +109,145 @@ function intro() {
   window.speechSynthesis.speak(speech);
 }
 
-// recording the content
-let meetingData = {};
-let finalData = {};
-function recordContent(value) {
-  var text;
-  let recognition = new SpeechRecognition();
-  recognition.start();
-  recognition.onstart = function () {
-    console.log('You can speak now!!!');
-  };
+// -------------------------------------------------------------------
 
-  recognition.onspeechend = function () {
-    // when user is done speaking
-    recognition.stop();
-  };
-
-  var content;
-  recognition.onresult = function (event) {
-    text = event.results[0][0].transcript;
-    content = text;
-    // document.getElementById('result').innerHTML = text;
-    console.log(content);
-    if (value == 1) {
-      finalData['senderEmailId'] = text;
-    } else if (value == 2) {
-      // text = text.charAt(0).toUpperCase() + text.slice(1);
-      finalData['mailSubject'] = text;
-    } else if (value == 3) {
-      finalData['mailText'] = text;
-    } else if (value == 4) {
-      meetingData['meetingType'] = text;
-    }
-  };
-}
-
+// Mail Content
+// Ask of EmailID, Subject and Content of mail
 function mailContent() {
   Speak('Tell the sender Mail Id');
-  recordContent(1);
-
-  setTimeout(function () {
-    Speak('Tell me the Mail Subject');
-    recordContent(2);
-  }, 14000);
-
-  setTimeout(function () {
-    Speak('Tell me the Mail Content');
-    recordContent(3);
-  }, 24000);
-
-  setTimeout(() => {
-    Speak('Ok ! Wait for few seconds send the mail..');
-    console.log(finalData);
-    localStorage.setItem('data', JSON.stringify(finalData));
-
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', '/sendmail', true);
-    xhr.send(JSON.stringify(finalData));
-
-  }, 45000);
+  Listen()
+    .then((data) => {
+      let text = data.results[0][0].transcript;
+      text = text.split(' ').join('').toLowerCase();
+      if (text.includes('attherate')) {
+        text = text.replace('attherate', '@');
+      }
+      finalData['senderEmailId'] = text;
+    })
+    .then(() => {
+      Speak('Tell me the Mail Subject');
+    })
+    .then(() => {
+      Listen()
+        .then((data) => {
+          let text = data.results[0][0].transcript;
+          text = text.charAt(0).toUpperCase() + text.slice(1);
+          console.log(text);
+          finalData['mailSubject'] = text;
+        })
+        .then(() => {
+          Speak('Tell me the Mail Content');
+        })
+        .then(() => {
+          Listen()
+            .then((data) => {
+              let text = data.results[0][0].transcript;
+              text = text.charAt(0).toUpperCase() + text.slice(1);
+              console.log(text + '.');
+              finalData['mailText'] = text + '.';
+            })
+            .then(() => {
+              Speak('Ok ! Wait for few seconds send the mail..');
+              console.log(finalData);
+            })
+            .then(() => {
+              axios({
+                url: '/sendMail',
+                method: 'POST',
+                data: finalData,
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-function editcontent() {
-  let text = finalData['senderEmailId'];
-  let Id;
-  text = text.split(' ').join('').toLowerCase();
-  if (text.includes('attherate')) {
-    text = text.replace('attherate', '@');
-  }
-  let gif = text.split('@');
-  if (gif[1].includes('gmail')) {
-    Id = gif[0] + '@gmail.com';
-  } else {
-    Id = gif[0] + '@yahoo.com';
-  }
-  finalData['senderEmailId'] = Id;
+// -------------------------------------------------------------------
 
-  text = finalData['mailSubject'];
-  text = text.charAt(0).toUpperCase() + text.slice(1);
-  finalData['mailSubject'] = text;
+// Arranging Different Meeting
+function meetingContent(data) {
+  if (data.includes('schedule')) {
+    Speak("What's the Date for the meeting..");
+    Listen()
+      .then((listen) => {
+        let text = listen.results[0][0].transcript;
+        text = text.toLowerCase();
+        console.log(text);
+
+        meetingData['meetingType'] = 'schedule';
+        meetingData['meetingDate'] = text;
+      })
+      .then(()=>{
+        return new Promise((resolve, reject)=>{
+          setTimeout(() => {
+            resolve();
+          }, 3000);
+        })
+      })
+      .then(() => {
+        Speak('What are the timings...');
+        Listen()
+          .then((voice) => {
+            let text = voice.results[0][0].transcript;
+            text = text.toLowerCase();
+            text = text.split('to');
+            let numb = text[0].match(/\d/g);
+            numb = numb.join('');
+            meetingData['meetingStartTime'] = numb;
+
+            numb = text[1].match(/\d/g);
+            numb = numb.join('');
+            meetingData['meetingStartTime'] = numb;
+          })
+          .then(() => {
+            Speak('Please Tell the meeting Guest Email one by one..');
+            meetingData['GuestList'] = [];
+          })
+          .then(() => {
+            let i = 0;
+            do {
+              Listen()
+                .then((value) => {
+                  let text = value.results[0][0].transcript;
+                  text = text.split(' ').join('').toLowerCase();
+                  if (text.includes('attherate')) {
+                    text = text.replace('attherate', '@');
+                  }
+                  meetingData['GuestList'].push(text);
+                })
+                .then(() => {
+                  Speak('Ok next..');
+                  i = i + 1;
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            } while (i < 5);
+          })
+          .then(() => {
+            Speak('Ok.. Scheduling a meet');
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    meetingData['meetingType'] = 'instant';
+    axios({
+      url: '/meeting',
+      method: 'POST',
+      data: meetingData,
+    });
+  }
 }
